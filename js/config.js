001699eb9fb4090bd4144b41efa3028345fc5683
Team-Lightning-LLC <lehmanachris@gmail.com>
2025-11-12 15:31:44 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -4,7 +4,7 @@ const CONFIG = {
   VERTESIA_API_BASE: 'https://api.vertesia.io/api/v1',
   VERTESIA_API_KEY: 'sk-7cec26efd3db9bb9f7909084a92864ba',
   ENVIRONMENT_ID: '681915c6a01fb262a410c161',
-  MODEL: 'publishers/anthropic/models/claude-sonnet-4-5',
+  MODEL: 'publishers/anthropic/models/claude-sonnet-4',
   
   // Research Agent Configuration
   INTERACTION_NAME: 'ResearchV2',
